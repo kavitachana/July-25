db02b846f88c611538a73072c8b7e12605a2e75e
kavitachana <kavitachana@gmail.com>
2016-07-25 22:37:28 -0400

    Assignement 2 - Getters and Setters 7/25/16

---
+++ AnswersToAssignment.java
@@ -0,0 +1,106 @@
+package com.tiy.week2.day1;
+
+public class AnswersToAssignment {
+
+	private float diskFileSize;
+	private double numberOfAcres;
+	private String fileName;
+	private String phoneNumber;
+	private String companyName;
+	private String todaysDate;
+	private boolean lightOnOrOff;
+	private double pi;
+	private double speedOfLight;
+	private Vehicle make;
+	private Address forSale;
+	private Product[] products;
+	
+	public float getDiskFileSize() {
+		return diskFileSize;
+	}
+	public void setDiskFileSize(float diskFileSize) {
+		this.diskFileSize = diskFileSize;
+	}
+	
+	public double getNumberOfAcres() {
+		return numberOfAcres;
+	}
+	public void setNumberOfAcres(double numberOfAcres) {
+		this.numberOfAcres = numberOfAcres;
+	}
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	public String getTodaysDate() {
+		return todaysDate;
+	}
+	public void setTodaysDate(String todaysDate) {
+		this.todaysDate = todaysDate;
+	}
+	
+	public boolean isLightOnOrOff() {
+		return lightOnOrOff;
+	}
+	public void setLightOnOrOff(boolean lightOnOrOff) {
+		this.lightOnOrOff = lightOnOrOff;
+	}
+	
+	public double getPi() {
+		return pi;
+	}
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+	
+	public double getSpeedOfLight() {
+		return speedOfLight;
+	}
+	public void setSpeedOfLight(double speedOfLight) {
+		this.speedOfLight = speedOfLight;
+	}
+	
+	public Vehicle getMake() {
+		return make;
+	}
+	public void setMake(Vehicle make) {
+		this.make = make;
+	}
+	
+	public Address getForSale() {
+		return forSale;
+	}
+	public void setForSale(Address forSale) {
+		this.forSale = forSale;
+	}
+	
+	public Product[] getProducts() {
+		return products;
+	}
+	public void setProducts(Product[] products) {
+		this.products = products;
+	}
+	
+	
+	
+	
+	
+}
